e233cd7cb9386a06e52b9dcf2e118e979897891e
OliveiradaCunha <oliveiradacunha@outlook.com>
2023-04-28 00:04:54 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!"
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo";
+
+// concatenaçao (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
